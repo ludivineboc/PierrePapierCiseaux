85eba598370902675b147529dc95b259eeb0f944
Ludivine Bocquier <98520169+ludivineboc@users.noreply.github.com>
2024-04-16 19:34:46 +0200

    javscript algorythm

---
+++ script.js
@@ -0,0 +1,59 @@
+let choixOrdi;
+let choixUtili;
+
+// Fonction choix de l'utilisateur
+function choixUtilisateur() {
+  // Role : Demander à l'utilisateur de choisir une option par le biais d'un prompt et transforme la reponse pour la mettre en majuscule 
+  //parametre : aucun
+  choixUtili = prompt("Choisissez pierre, Papier ou Ciseaux :").toUpperCase();
+  return choixUtili;
+}
+
+// fonction choix de l'ordinateur
+function choixOrdinateur() {
+    // Role : Générer un nombre aléatoire entre 0 et 2 pour représenter le choix de l'ordinateur. Le chiffre est ensuite associé à une chaine de caractère
+  // parametre : aucun
+  var choixOrdiNum = Math.floor(Math.random() * 3); // 0: pierre, 1: Papier, 2: Ciseaux
+
+  // Convertir le nombre en une chaîne de caractères
+  if (choixOrdiNum === 0) {
+    choixOrdi = "PIERRE";
+  } else if (choixOrdiNum === 1) {
+    choixOrdi = "PAPIER";
+  } else {
+    choixOrdi = "CISEAUX";
+  }
+
+  return choixOrdinateur;
+}
+
+// fonction afficher les choix
+function afficherChoix() {
+  alert(`Vous avez choisi :  ${choixUtili} 
+    L'ordinateur a choisi : ${choixOrdi}`);
+}
+
+// fonction determiner le gagnant
+function determinerGagnant() {
+  if (choixUtili === choixOrdi) {
+    alert("Égalité !");
+  } else if (
+    (choixUtili === "PIERRE" && choixOrdi === "CISEAUX") ||
+    (choixUtili === "PAPIER" && choixOrdi === "PIERRE") ||
+    (choixUtili === "CISEAUX" && choixOrdi === "PAPIER")
+  ) {
+    alert("Vous avez gagné !");
+  } else {
+    alert("L'ordinateur a gagné !");
+  }
+}
+
+//Focntion jouer pour lancer mon jeu
+function jouer() {
+  choixUtilisateur();
+  choixOrdinateur();
+  afficherChoix();
+  determinerGagnant();
+}
+
+jouer();
